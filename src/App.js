448e97a98b1664a9e3abc37e3c8bfad3bb202240
mattcbodily <mattcbodily@gmail.com>
2020-04-21 16:18:17 -0600

    added sass for styling, started build Dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Dashboard from './Components/Dashboard/Dashboard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Dashboard />
     </div>
   );
 }
