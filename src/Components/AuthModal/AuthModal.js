1d3702709137869074751c261b39c926ca17ff52
mattcbodily <mattcbodily@gmail.com>
2020-04-29 16:44:30 -0600

    adding switch statement to auth-modal to handle rendering of registration steps

--- src/Components/AuthModal/AuthModal.js
+++ src/Components/AuthModal/AuthModal.js
@@ -2,39 +2,58 @@ import React, {useState} from 'react';
 import './AuthModal.scss';
 
 export default props => {
-    const [firstName, setFirstName] = useState(''),
+    const [step, setStep] = useState(1),
+          [firstName, setFirstName] = useState(''),
           [lastName, setLastName] = useState(''),
           [email, setEmail] = useState(''),
           [password, setPassword] = useState(''),
           [verPassword, setVerPassword] = useState('');
 
+    const registerSteps = () => {
+        switch(step){
+            case 1:
+                return (
+                    <section className='auth-modal'>
+                        <svg id='close-icon' onClick={props.toggleFn} xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle">
+                            <circle cx="12" cy="12" r="10"></circle>
+                            <line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line>
+                        </svg>
+                        <h3>Create Your Account</h3>
+                        <input 
+                            value={firstName}
+                            placeholder='First Name'
+                            onChange={(e) => setFirstName(e.target.value)}/>
+                        <input 
+                            value={lastName}
+                            placeholder='Last Name'
+                            onChange={(e) => setLastName(e.target.value)}/>
+                        <input 
+                            value={email}
+                            placeholder='Email'
+                            onChange={(e) => setEmail(e.target.value)}/>
+                        <input 
+                            type='password'
+                            value={password}
+                            placeholder='Password'
+                            onChange={(e) => setPassword(e.target.value)}/>
+                        <input
+                            type='password' 
+                            value={verPassword}
+                            placeholder='Verify Password'
+                            onChange={(e) => setVerPassword(e.target.value)}/>
+                        <button onClick={() => setStep(2)}>Next</button>
+                    </section>
+                )
+            case 2:
+                return;
+            default:
+                return;
+        }
+    }
+
     return (
         <div className='auth-modal-backdrop'>
-            <section className='auth-modal'>
-                <input 
-                    value={firstName}
-                    placeholder='First Name'
-                    onChange={(e) => setFirstName(e.target.value)}/>
-                <input 
-                    value={lastName}
-                    placeholder='Last Name'
-                    onChange={(e) => setLastName(e.target.value)}/>
-                <input 
-                    value={email}
-                    placeholder='Email'
-                    onChange={(e) => setEmail(e.target.value)}/>
-                <input 
-                    type='password'
-                    value={password}
-                    placeholder='Password'
-                    onChange={(e) => setPassword(e.target.value)}/>
-                <input
-                    type='password' 
-                    value={verPassword}
-                    placeholder='Verify Password'
-                    onChange={(e) => setVerPassword(e.target.value)}/>
-                <button>Next</button>
-            </section>
+            {registerSteps()}
         </div>
     )
 
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -10,7 +10,7 @@ const Landing = props => {
                 <h1>Welcome to Preceptive</h1>
                 <h3>The all-in-one platform for tutors and private instructors</h3>
                 <button onClick={props.toggleObj.toggleFn}>Get Started</button>
-                {props.toggleObj.value ? <AuthModal /> : null}
+                {props.toggleObj.value ? <AuthModal toggleFn={props.toggleObj.toggleFn} /> : null}
             </section>
             <section>
                 This section will explain what we do
