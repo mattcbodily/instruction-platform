2548c4453e935af8745c4e8adb0dfb61bf2baa61
mattcbodily <mattcbodily@gmail.com>
2020-04-29 11:02:15 -0600

    added inputs to AuthModal

--- src/Components/AuthModal/AuthModal.js
+++ src/Components/AuthModal/AuthModal.js
@@ -1,10 +1,41 @@
 import React, {useState} from 'react';
 import './AuthModal.scss';
 
-export default props => (
-    <div className='auth-modal-backdrop'>
-        <section className='auth-modal'>
+export default props => {
+    const [firstName, setFirstName] = useState(''),
+          [lastName, setLastName] = useState(''),
+          [email, setEmail] = useState(''),
+          [password, setPassword] = useState(''),
+          [verPassword, setVerPassword] = useState('');
 
-        </section>
-    </div>
-)
+    return (
+        <div className='auth-modal-backdrop'>
+            <section className='auth-modal'>
+                <input 
+                    value={firstName}
+                    placeholder='First Name'
+                    onChange={(e) => setFirstName(e.target.value)}/>
+                <input 
+                    value={lastName}
+                    placeholder='Last Name'
+                    onChange={(e) => setLastName(e.target.value)}/>
+                <input 
+                    value={email}
+                    placeholder='Email'
+                    onChange={(e) => setEmail(e.target.value)}/>
+                <input 
+                    type='password'
+                    value={password}
+                    placeholder='Password'
+                    onChange={(e) => setPassword(e.target.value)}/>
+                <input
+                    type='password' 
+                    value={verPassword}
+                    placeholder='Verify Password'
+                    onChange={(e) => setVerPassword(e.target.value)}/>
+                <button>Next</button>
+            </section>
+        </div>
+    )
+
+}
