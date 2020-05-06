2ef8e598fd3e37061ab66c78645dfb255348517c
mattcbodily <mattcbodily@gmail.com>
2020-05-06 16:29:51 -0600

    added basic display for the courses module

--- src/Components/Dashboard/DashModules/CourseModule/CourseModule.js
+++ src/Components/Dashboard/DashModules/CourseModule/CourseModule.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './CourseModule.scss';
 
-const CourseModule = props => (
-    <div className='course-module'>Course Module</div>
-)
+const CourseModule = props => {
+    const [courses, setCourses] = useState([]),
+          [courseName, setCourseName] = useState(''),
+          [description, setDescription] = useState('');
+
+    return (
+        <div className='course-module'>
+            {courses.length
+            ? null
+            : (<>
+                <p>You don't have any courses!</p>
+                <button>Create one here</button>
+               </>)}
+        </div>
+    )
+}
+
 
 export default CourseModule;
